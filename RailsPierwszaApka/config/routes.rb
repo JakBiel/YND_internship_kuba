8eb38e63cc7331d8bd4e8f6f4a8e1b4ab59c0108
JakBiel <jakbiel@gmail.com>
2020-01-10 14:03:29 +0100

    lekkie uzupelnienie poprzedniego commita w routach

--- RailsPierwszaApka/config/routes.rb
+++ RailsPierwszaApka/config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   get 'projects/show'
 
   get 'welcome/index'
-  resources :projects, only [:index, :new, :create, :show] do
-    resources :to_do_list, only [:index, :new, :create, :show] do
-      resources :task, only [:index, :new, :create, :show] do
-        resources :comment, only [:index, :new, :create, :show]
+  resources :projects, only [:index, :create, :show] do
+    resources :to_do_list, only [:index, :create, :show] do
+      resources :task, only [:index, :create, :show] do
+        resources :comment, only [:index, :create, :show]
       end
     end
   end
   root 'welcome#index'
-
-  #usun wszedzie new
+  
 end
