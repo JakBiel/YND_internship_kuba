af25ac7875bd12a9f573bda99194cdc26cf5fb4d
JakBiel <jakbiel@gmail.com>
2020-01-16 10:46:00 +0100

    Dodanie nameów do Tasków

--- RailsPierwszaApka/db/migrate/20200110112038_create_tasks.rb
+++ RailsPierwszaApka/db/migrate/20200110112038_create_tasks.rb
@@ -1,6 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
+      t.string :name
       t.string :description
       t.date :due_date
       t.integer :position
