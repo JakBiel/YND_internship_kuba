26c678f37d7948325ca6b1e91965c178bf8d5f70
JakBiel <jakbiel@gmail.com>
2020-01-17 15:08:15 +0100

    sa trzy testy jednostkowe na Projectsach

--- RailsPierwszaApka/spec/factories.rb
+++ RailsPierwszaApka/spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :project do
-    name { "John" }
-    description  { "Jakistamopisik_1_test_Rspecowy" }
+    name { "Johnasdfdsl" }
+    description  { "Test" }
   end
 end
--- RailsPierwszaApka/spec/models/project_spec.rb
+++ RailsPierwszaApka/spec/models/project_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  context "Name analyzing" do
-    it "is fewer than 64 characters?" do
+  context "by analyzing <<Description>>" do
+    it "should be made of fewer than 64 characters" do
       obiekt_project1 = create(:project)
-      expect(obiekt_project1.name.size).to be < 64
+      expect(obiekt_project1.description.size).to be < 64
+    end
+    it "should be the same as string <<Test>>" do
+      obiekt_project1 = create(:project)
+      expect(obiekt_project1.description.to_s) == "Test"
+    end
+    it "should contain string element <<Te>>" do
+      obiekt_project1 = create(:project)
+      obiekt_project1.description.should include("Te")
     end
   end
 end
+
