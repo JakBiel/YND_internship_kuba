320a30218d1941f5aa1912b2012fe02d54627f46
JakBiel <jakbiel@gmail.com>
2020-02-04 14:56:38 +0100

    Poprawienie CORSa o DELETE'a; usuniecie na prosbe frontendow paginacji z Taskow

--- RailsPierwszaApka/app/controllers/tasks_controller.rb
+++ RailsPierwszaApka/app/controllers/tasks_controller.rb
@@ -2,16 +2,25 @@ class TasksController < ApplicationController
 
   def index
 
-    size_number = 3
+    #size_number = 3
+    #
+    #if params[:done_status] == nil
+    #  @tasks = Task.where(to_do_list_id: params[:to_do_list_id]).all.page(params[:page]).per(size_number)
+    #else
+    #  @tasks = Task.where(done_status: params[:done_status], to_do_list_id: params[:to_do_list_id]).all.page(params[:page]).per(size_number)
+    #end
+    #
+    #  #render json: @tasks
+    #render json: { tasks: @tasks, meta: meta_data(@tasks) }
+
 
     if params[:done_status] == nil
-      @tasks = Task.where(to_do_list_id: params[:to_do_list_id]).all.page(params[:page]).per(size_number)
+      @tasks = Task.where(to_do_list_id: params[:to_do_list_id]).all
     else
-      @tasks = Task.where(done_status: params[:done_status], to_do_list_id: params[:to_do_list_id]).all.page(params[:page]).per(size_number)
+      @tasks = Task.where(done_status: params[:done_status], to_do_list_id: params[:to_do_list_id]).all
     end
 
-      #render json: @tasks
-    render json: { tasks: @tasks, meta: meta_data(@tasks) }
+    render json: @tasks
   end
 
   def show
