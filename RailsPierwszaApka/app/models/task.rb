79307e10b10c8efb2e670446ef5c2e8d3c150a47
JakBiel <jakbiel@gmail.com>
2020-02-05 16:59:35 +0100

    Wyłączenie walidacji modelu na Taskach, bo krzaczy na frontendzie

--- RailsPierwszaApka/app/models/task.rb
+++ RailsPierwszaApka/app/models/task.rb
@@ -1,10 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :to_do_list
 
-  validates_length_of :name, :minimum => 6
-  validates_numericality_of :position, :greater_than => 0
+  #validates_length_of :name, :minimum => 6
+  #validates_numericality_of :position, :greater_than => 0
   #validates_inclusion_of :done_status, :in => %w(true false),
   #                                        :message => "Value of done_status is not a valid boolean option"
-  validates_exclusion_of :done_status, :in => %w(true),
-                         :message => "Done_status should be <<false>> in moment of creation by default."
+  #validates_exclusion_of :done_status, :in => %w(true),
+  #                       :message => "Done_status should be <<false>> in moment of creation by default."
 end
