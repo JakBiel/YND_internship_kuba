51da43f78ea6837775b0b4e9dbb760d26cc70a3d
JakBiel <jakbiel@gmail.com>
2020-02-05 16:21:25 +0100

    Poprawka na CORSach, zmiana portu z 3006 na 3005

--- RailsPierwszaApka/config/application.rb
+++ RailsPierwszaApka/config/application.rb
@@ -15,7 +15,7 @@ module RailsPierwszaApka
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3006', '127.0.0.1:3006', /http:\/\/192\.168\.0\.\d{1,3}(:\d+)?/
+        origins 'localhost:3005', '127.0.0.1:3005', /http:\/\/192\.168\.0\.\d{1,3}(:\d+)?/
         resource '*', headers: :any, methods: [:get, :post, :options, :patch, :delete]
       end
     end
