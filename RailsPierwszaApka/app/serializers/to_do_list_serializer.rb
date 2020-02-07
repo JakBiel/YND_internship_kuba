ec9158a3626dcc99e2783c06dec64bc30013b0dc
JakBiel <jakbiel@gmail.com>
2020-02-07 11:42:38 +0100

    Zrobione wyswietlanie w to_do_listach 3 rzeczy: zwyklwych paramsów todolist, lokalnych paramsow todolistowych informujacych o taskach (na prosbe frontów) oraz metadane

--- RailsPierwszaApka/app/serializers/to_do_list_serializer.rb
+++ RailsPierwszaApka/app/serializers/to_do_list_serializer.rb
@@ -1,18 +1,18 @@
-#class ToDoListSerializer < ActiveModel::Serializer
-#  attributes :id, :name, :project_id, :completed_tasks, :all_tasks
-#
-#  #has_many :tasks
-#
-#  def completed_tasks
-#    Task.where(done_status: false, to_do_list_id: object.id).all.count
-#  end
-#
-#  def all_tasks
-#    Task.where(to_do_list_id: object.id).all.count
-#  end
-#
-#  #def as_json(options={})
-#  #  super(only: [:id, :name, :project_id, :completed_tasks, :all_tasks])
-#  #end
-#
-#end
+class ToDoListSerializer < ActiveModel::Serializer
+  attributes :id, :name, :project_id, :completed_tasks, :all_tasks
+
+  #has_many :tasks
+
+  def completed_tasks
+    Task.where(done_status: false, to_do_list_id: object.id).all.count
+  end
+
+  def all_tasks
+    Task.where(to_do_list_id: object.id).all.count
+  end
+
+  #def as_json(options={})
+  #  super(only: [:id, :name, :project_id, :completed_tasks, :all_tasks])
+  #end
+
+end
