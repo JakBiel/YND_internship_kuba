c01aa2a708df151b6915c6a504f19a4f64059260
JakBiel <jakbiel@gmail.com>
2020-02-07 15:42:10 +0100

    Testowe zmodyfikowanie wadliwie (poki co) dzialajacego seeda

--- RailsPierwszaApka/db/seeds.rb
+++ RailsPierwszaApka/db/seeds.rb
@@ -8,10 +8,10 @@ kolekcja1.each do |numer1|
   kolekcja2.each do |numer2|
     ToDoList.create("name"=>"Moja_todolista_#{numer2}", "project_id"=>numer1)
     kolekcja3.each do |numer3|
-      Task.create("name"=>"Moje_imie_nr_#{numer3}", "description"=>"Opis mojego taska nr #{numer3}", "due_date"=>"2020-09-20", "position"=>numer3+1, "done_status"=>true, "to_do_list_id"=>numer2)
+      Task.create("name"=>"Moje_imie_nr_#{numer3}", "description"=>"Opis mojego taska nr #{numer3}", "due_date"=>"2#{numer1}#{numer2}#{numer3}-09-20", "position"=>numer3+1, "done_status"=>true, "to_do_list_id"=>numer2)
     end
     kolekcja4.each do |numer4|
-      Comment.create("user_name"=>"Taki username_#{numer4}", "post_date"=>"2000-04-03", "body"=>"Moj_maly_komentarz_#{numer4}", "file"=>"Tekst#{numer4}" ,"to_do_list_id"=>numer2)
+      Comment.create("user_name"=>"Taki username_#{numer4}", "post_date"=>"2#{numer1}#{numer2}#{numer4}-04-03", "body"=>"Moj_maly_komentarz_#{numer4}", "file"=>"Tekst#{numer4}" ,"to_do_list_id"=>numer2)
     end
   end
 end
