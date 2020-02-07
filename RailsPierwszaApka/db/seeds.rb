c832f9d3efdf8b34b05b50953dd82f483f133b2c
JakBiel <jakbiel@gmail.com>
2020-02-07 16:30:53 +0100

    Kolejna poprawka automatycznego seeda

--- RailsPierwszaApka/db/seeds.rb
+++ RailsPierwszaApka/db/seeds.rb
@@ -6,7 +6,7 @@ kolekcja4 = (1..4) #do commentsow
 kolekcja1.each do |numer1|
   Project.create("name"=>"Nazwa_nr_#{numer1}", "description"=>"Opis_nr_#{numer1}")
   kolekcja2.each do |numer2|
-    ToDoList.create("name"=>"Moja_todolista_#{numer2}", "project_id"=>numer1)
+    ToDoList.create("name"=>"Moja_todolista_#{numer2}", "project_id"=>numer2)
     kolekcja3.each do |numer3|
       Task.create("name"=>"Moje_imie_nr_#{numer3}", "description"=>"Opis mojego taska nr #{numer3}", "due_date"=>"2#{numer1}#{numer2}#{numer3}-09-20", "position"=>numer3+1, "done_status"=>true, "to_do_list_id"=>numer2)
     end
